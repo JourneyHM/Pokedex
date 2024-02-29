92d2bbad61f206d4feb4070da139433d9c10d029
JourneyHM <a01662556@tec.mx>
2024-02-29 03:04:07 -0600

    Base Pokedex
    Diseño contorno de la Pokedex

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -2,24 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
-function App() {
 
+function App() {
   const [contador, setContador]= React.useState(0);
 
   function aumentar(){
     setContador(contador+1);
     console.log(contador);
   }
+
   function disminuir(){
     setContador(contador-1);
     console.log(contador);
   }
+
   return (
     <div className="App">
-      <h1>HELLO WORLD {contador}</h1>
-      <Button onClick={aumentar} label="Aumentar" color='blue'></Button>
-      <Button onClick={disminuir} label="Disminuir" color='red'></Button>
-
+      <div className="flex flex-row">
+        <div className="basis-5/6 flex flex-col">
+          <div className="rounded-lg border-8 border-pink-950 bg-rose-700 h-48 mt-20 ml-20"></div>
+          <div className="rounded-lg border-8 border-pink-950 bg-rose-800 h-dvh ml-20"></div>
+        </div>
+        <div className="basis-1/12 mt-16 mb-20 flex-grow-0">
+          <div className="flex flex-col" style={{ height: '100%' }}>
+            <div className="h-48 rounded-full border-8 border-pink-950 bg-rose-700" style={{ flex: 1 }}>
+            </div>
+          </div>
+        </div>
+        <div className="basis-5/6 flex flex-col">
+          <div className="bg-white h-48 mt-20 mr-20"></div>
+          <div className="rounded-lg border-8 border-pink-950 bg-rose-700 h-dvh mb-20 mr-20"></div>
+        </div>
+      </div>
     </div>
   );
 }
