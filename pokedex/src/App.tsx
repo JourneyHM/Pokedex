628c525b66ed79af50e574a0295813e393505809
JourneyHM <a01662556@tec.mx>
2024-02-29 18:10:41 -0600

    Fondo y base bien potente

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -17,21 +17,16 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App bg-yellow-300">
       <div className="flex flex-row">
-        <div className="basis-5/6 flex flex-col">
+        <div className="basis-1/2 flex flex-col">
           <div className="rounded-lg border-8 border-pink-950 bg-rose-700 h-48 mt-20 ml-20"></div>
           <div className="rounded-lg border-8 border-pink-950 bg-rose-800 h-dvh ml-20"></div>
         </div>
-        <div className="basis-1/12 mt-16 mb-20 flex-grow-0">
-          <div className="flex flex-col" style={{ height: '100%' }}>
-            <div className="h-48 rounded-full border-8 border-pink-950 bg-rose-700" style={{ flex: 1 }}>
-            </div>
+        <div className="basis-1/2 flex flex-col">
+          <div className="bg-yellow-300 h-48 mt-20 mr-20"></div>
+          <div className="rounded-lg border-8 border-pink-950 bg-rose-700 h-dvh mb-20 mr-20">
           </div>
-        </div>
-        <div className="basis-5/6 flex flex-col">
-          <div className="bg-white h-48 mt-20 mr-20"></div>
-          <div className="rounded-lg border-8 border-pink-950 bg-rose-700 h-dvh mb-20 mr-20"></div>
         </div>
       </div>
     </div>
