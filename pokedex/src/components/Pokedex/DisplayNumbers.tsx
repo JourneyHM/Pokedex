40ba3920ce7ab186210a6b2d971ab6ee4478f33d
AndreaAguilar <a01656200@tec.mx>
2024-03-01 16:44:45 -0600

    ahora si cambios

--- pokedex/src/components/Pokedex/DisplayNumbers.tsx
+++ pokedex/src/components/Pokedex/DisplayNumbers.tsx
@@ -1,15 +1,11 @@
-import { lazy } from 'react';
 import './displayNumbers.css'
 
 interface DisplayProps{
     color:string|'disColor'
-    label:string;
 }
 
 export default function DisplayNumbers(props:DisplayProps){
     return(
-        <div className={`display ${props.color==='disColor'?'disColor':''}`}>
-            {props.label}
-        </div>
+        <div className={`display ${props.color==='disColor'?'disColor':''}`}></div>
     )
 }
