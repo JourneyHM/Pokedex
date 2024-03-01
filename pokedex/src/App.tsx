f2eae69c12dd602d830c7f1ad950be75e12246c0
FranciscoSanchezz <117677307+FranciscoSanchezz@users.noreply.github.com>
2024-02-29 18:07:22 -0600

    Solo le faltan los margenes

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -20,21 +20,25 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-row">
-      <Container color='red'></Container>
         <div className="basis-5/6 flex flex-col">
           <div className="rounded-lg border-8 border-pink-950 bg-rose-700 h-48 mt-20 ml-20"></div>
           
-          <div className="rounded-lg border-8 border-pink-950 bg-rose-800 h-dvh ml-20"></div>
+          <div className="rounded-lg border-8 border-pink-950 bg-rose-800 h-dvh ml-20">
+            <Container color='blue'></Container>
+          </div>
+          
         </div>
         <div className="basis-1/12 mt-16 mb-20 flex-grow-0">
           <div className="flex flex-col" style={{ height: '100%' }}>
             <div className="h-48 rounded-full border-8 border-pink-950 bg-rose-700" style={{ flex: 1 }}>
+              
             </div>
           </div>
         </div>
         <div className="basis-5/6 flex flex-col">
           <div className="bg-white h-48 mt-20 mr-20"></div>
-          <div className="rounded-lg border-8 border-pink-950 bg-rose-700 h-dvh mb-20 mr-20"></div>
+          <div className="rounded-lg border-8 border-pink-950 bg-rose-700 h-dvh mb-20 mr-20">
+          </div>
         </div>
       </div>
     </div>
--- pokedex/src/components/Pokedex/Container.tsx
+++ pokedex/src/components/Pokedex/Container.tsx
@@ -1,12 +1,12 @@
 import './container.css';
 
 interface ContainerProps{
-    color:string|'white'|'red';
+    color:string|'white'|'red'|'blue';
 }
 
 export default function Container(props:ContainerProps){
     return(
-        <div className={`container ${props.color==='white'?'white':''}${props.color==='red'?'red':''}` }
+        <div className={`container ${props.color==='white'?'white':''}${props.color==='red'?'red':''}${props.color==='blue'?'blue':''}` }
         ></div>
     )
 }
